c8df08cb2d320f0140e29bca80d5ce95723435d2
Githerd <158222320+Githerd@users.noreply.github.com>
2024-10-15 12:58:53 -0700

    Update signals.py

--- diet_tracker/users/signals.py
+++ diet_tracker/users/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth import get_user_model
+from django.contrib.auth import user
 from .models import Profile
 
 User = get_user_model()
