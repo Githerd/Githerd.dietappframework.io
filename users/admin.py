8ebbdaccf00e2db74bc971a049ec4a370d0f7a83
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-08 14:54:09 -0800

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, UserProfile, Profile, DietApp, Meal, Exercise, HealthData, TDEE, Weekly, Message, JournalEntry
+from .models import User, UserProfile, Profile, DietApp
 
 
 # Register the custom User model
@@ -32,59 +32,3 @@ class DietAppAdmin(admin.ModelAdmin):
     list_display = ['user', 'activity_level', 'goal', 'created_at']
     list_filter = ['activity_level', 'goal']
     search_fields = ['user__username', 'goal']
-
-
-# Register the Meal model
-@admin.register(Meal)
-class MealAdmin(admin.ModelAdmin):
-    list_display = ['user', 'name', 'calories', 'date']
-    list_filter = ['date']
-    search_fields = ['name', 'user__username']
-
-
-# Register the Exercise model
-@admin.register(Exercise)
-class ExerciseAdmin(admin.ModelAdmin):
-    list_display = ['user', 'name', 'type', 'duration', 'calories_burned', 'date']
-    list_filter = ['type', 'date']
-    search_fields = ['name', 'user__username']
-
-
-# Register the HealthData model
-@admin.register(HealthData)
-class HealthDataAdmin(admin.ModelAdmin):
-    list_display = ['user', 'weight', 'height', 'calories_intake', 'calories_burned', 'date']
-    search_fields = ['user__username']
-    list_filter = ['date']
-
-
-# Register the TDEE model
-@admin.register(TDEE)
-class TDEEAdmin(admin.ModelAdmin):
-    list_display = ['user', 'calories', 'date']
-    search_fields = ['user__username']
-    list_filter = ['date']
-
-
-# Register the Weekly model
-@admin.register(Weekly)
-class WeeklyAdmin(admin.ModelAdmin):
-    list_display = ['meal', 'day', 'mealuser']
-    search_fields = ['meal__name', 'mealuser__username']
-    list_filter = ['day']
-
-
-# Register the Message model
-@admin.register(Message)
-class MessageAdmin(admin.ModelAdmin):
-    list_display = ['sender', 'receiver', 'timestamp']
-    search_fields = ['sender__username', 'receiver__username']
-    list_filter = ['timestamp']
-
-
-# Register the JournalEntry model
-@admin.register(JournalEntry)
-class JournalEntryAdmin(admin.ModelAdmin):
-    list_display = ['title', 'author', 'date_posted']
-    search_fields = ['title', 'author__username']
-    list_filter = ['date_posted']
