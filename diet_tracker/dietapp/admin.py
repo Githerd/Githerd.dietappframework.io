a567f5e1f3930fb745ecef9954644df2498eda52
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-08 11:23:28 -0800

    Update admin.py

--- diet_tracker/dietapp/admin.py
+++ diet_tracker/dietapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Meal, UserProfile, Post
+from .models import Meal, UserProfile, Exercise, Healthdata
 
 admin.site.register(Meal)
 admin.site.register(UserProfile)
-admin.site.register(Post)
+admin.site.register(Exercise)
+admin.site.register(Healthdata)
