1931c8ad66fdba1f7caa6d67b4a6137a2f9ad956
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-08 15:29:02 -0800

    Update models.py

--- users/models.py
+++ users/models.py
@@ -89,6 +89,16 @@ class Profile(models.Model):
             pass
 
 
+class JournalEntry(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Title")
+    content = models.TextField(verbose_name="Content")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="journal_entries")
+    date_posted = models.DateTimeField(default=now, verbose_name="Date Posted")
+
+    def __str__(self):
+        return self.title
+
+
 # Signal to create UserProfile and Profile automatically
 @receiver(post_save, sender=User)
 def create_and_save_user_profiles(sender, instance, created, **kwargs):
