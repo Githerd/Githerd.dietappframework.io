cd868846e3a025f58c3a1a78eba79ca6acd87bf0
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-08 14:42:49 -0800

    Update admin.py

--- dietapp/admin.py
+++ dietapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, UserProfile, Profile, DietApp, Exercise, HealthData, TDEE, Weekly, Message, JournalEntry
-from dietapp.models import Meal  
+from .models import User, UserProfile, Meal, Exercise, HealthData, TDEE, Weekly, Message, JournalEntry
 
+
+admin.site.register(User)
 admin.site.register(UserProfile)
 admin.site.register(Meal)
 admin.site.register(Exercise)
@@ -9,3 +10,4 @@ admin.site.register(HealthData)
 admin.site.register(TDEE)
 admin.site.register(Weekly)
 admin.site.register(Message)
+admin.site.register(JournalEntry)
