f79e6b1de6b1148d209ad159f7c2501df4811a65
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-08 11:59:06 -0800

    Update views.py

--- diet_tracker/polls/views.py
+++ diet_tracker/polls/views.py
@@ -1,101 +1,26 @@
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import get_object_or_404, render, redirect
-from django.urls import reverse
-from django.contrib.auth.decorators import login_required
-from .models import Meal
-from .forms import MealForm  # Assuming a MealForm is created for meal creation/updating
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 
+from .models import Question
 
-@login_required
 def index(request):
-    """
-    Displays the latest meals added by the user.
-    """
-    latest_meal_list = Meal.objects.filter(user=request.user).order_by("-date")[:5]
-    context = {"latest_meal_list": latest_meal_list}
-    return render(request, "dietapp/home.html", context)
-
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/home.html", context)
 
 def about(request):
-    """
-    Renders the About page for the Diet App.
-    """
-    return render(request, 'dietapp/about.html', {'title': 'About'})
-
+    return render(request, 'polls/about.html', {'title': 'About'})
 
 def contact(request):
-    """
-    Renders the Contact page for the Diet App.
-    """
-    return render(request, 'dietapp/contact.html', {'title': 'Contact'})
+    return render(request, 'polls/contact.html', {'title': 'contact'})
 
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
-@login_required
-def detail(request, meal_id):
-    """
-    Displays the details of a specific meal.
-    """
-    meal = get_object_or_404(Meal, pk=meal_id, user=request.user)
-    return render(request, "dietapp/detail.html", {"meal": meal})
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
 
-
-@login_required
-def results(request, meal_id):
-    """
-    Placeholder view for displaying the results of a meal analysis.
-    """
-    meal = get_object_or_404(Meal, pk=meal_id, user=request.user)
-    return render(request, "dietapp/results.html", {"meal": meal})
-
-
-@login_required
-def vote(request, meal_id):
-    """
-    Placeholder view for handling meal voting or rating.
-    """
-    return HttpResponse("You're rating the meal %s." % meal_id)
-
-
-@login_required
-def create_meal(request):
-    """
-    Allows the user to create a new meal.
-    """
-    if request.method == "POST":
-        form = MealForm(request.POST)
-        if form.is_valid():
-            meal = form.save(commit=False)
-            meal.user = request.user
-            meal.save()
-            return redirect('dietapp-home')
-    else:
-        form = MealForm()
-    return render(request, "dietapp/create_meal.html", {"form": form})
-
-
-@login_required
-def edit_meal(request, meal_id):
-    """
-    Allows the user to edit an existing meal.
-    """
-    meal = get_object_or_404(Meal, pk=meal_id, user=request.user)
-    if request.method == "POST":
-        form = MealForm(request.POST, instance=meal)
-        if form.is_valid():
-            form.save()
-            return redirect('meal-detail', meal_id=meal.id)
-    else:
-        form = MealForm(instance=meal)
-    return render(request, "dietapp/edit_meal.html", {"form": form})
-
-
-@login_required
-def delete_meal(request, meal_id):
-    """
-    Deletes a meal for the logged-in user.
-    """
-    meal = get_object_or_404(Meal, pk=meal_id, user=request.user)
-    if request.method == "POST":
-        meal.delete()
-        return redirect('dietapp-home')
-    return render(request, "dietapp/delete_meal.html", {"meal": meal})
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
