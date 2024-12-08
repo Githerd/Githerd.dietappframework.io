9f6b8d8ae5414f08b982acd7283d0d46640bcd54
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-08 15:12:27 -0800

    Update settings.py

--- diet_tracker/settings.py
+++ diet_tracker/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key')
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['githerd-dietappframework-io.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
