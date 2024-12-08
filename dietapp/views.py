95f69a98883d94c843c852d3aa8718f1eb67cd04
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-08 15:51:16 -0800

    Update views.py

--- dietapp/views.py
+++ dietapp/views.py
@@ -10,6 +10,7 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView, D
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.utils import timezone
+from django.utils.timezone import now
 
 
 # Basic Views
