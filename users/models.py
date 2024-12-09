3d0358f90e5908a1d768eca4713a2e0c32af2fc5
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-09 13:58:35 -0800

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,23 +1,44 @@
 from django.db import models
-from django.contrib.auth.models import User
-from PIL import Image
+from django.contrib.auth.models import AbstractUser
+from django.utils.timezone import now
+from django.core.validators import FileExtensionValidator, MinValueValidator
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+# Custom User model
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.user.username} Profile'
+        return self.username
 
-    """
-    Changes here explained below
-    """
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
 
-        img = Image.open(self.image.path)
+# UserProfile model
+class UserProfile(models.Model):
+    USER_GOALS = [
+        ('lose_weight', 'Lose Weight'),
+        ('gain_weight', 'Gain Weight'),
+        ('maintain_weight', 'Maintain Weight'),
+    ]
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    age = models.PositiveIntegerField(null=True, blank=True)
+    height = models.FloatField(null=True, blank=True, validators=[MinValueValidator(0)])
+    weight = models.FloatField(null=True, blank=True, validators=[MinValueValidator(0)])
+    goal = models.CharField(max_length=50, choices=USER_GOALS, default='maintain_weight')
+    image = models.ImageField(
+        upload_to='profile_pics/',
+        default='default.jpg',
+        validators=[FileExtensionValidator(['jpg', 'jpeg', 'png'])]
+    )
 
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+
+    @property
+    def bmi(self):
+        """Calculate and return the Body Mass Index (BMI)."""
+        if self.height and self.weight:
+            height_in_meters = self.height / 100
+            return round(self.weight / (height_in_meters ** 2), 2)
+        return None
