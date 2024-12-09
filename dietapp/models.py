10aefd04890a55fb5ad9ffa0d7b244f787c3a67c
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-08 19:24:43 -0800

    Update models.py

--- dietapp/models.py
+++ dietapp/models.py
@@ -7,7 +7,8 @@ from django.contrib.auth.models import User, AbstractUser
 from django.contrib.auth import get_user_model
 
 
-User = get_user_model()
+class User(AbstractUser):
+    pass
 
 
 # Choices
