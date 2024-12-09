546f74c59bcaaacca1e0684cf48d747bfbd7d570
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-09 05:35:39 -0800

    Update models.py

--- users/models.py
+++ users/models.py
@@ -2,6 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 
+
+class Meal(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="meals")
+    name = models.CharField(max_length=100)
+    calories = models.FloatField(default=0.0)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
