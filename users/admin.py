cd596866355c60f3f8c4e7c573df59945ad2b849
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-08 18:46:36 -0800

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,23 +1,5 @@
 from django.contrib import admin
-from .models import User, UserProfile, Profile
+from .models import Profile
 
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ['username', 'email', 'is_staff', 'date_joined']
-    search_fields = ['username', 'email']
-    list_filter = ['is_staff', 'is_active', 'date_joined']
-    ordering = ['-date_joined']
-
-
-@admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ['user', 'age', 'height', 'weight', 'goal']
-    search_fields = ['user__username', 'goal']
-    list_filter = ['age', 'goal']
-
-
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ['user', 'image']
-    search_fields = ['user__username']
+# Register your models here.
+admin.site.register(Profile)
