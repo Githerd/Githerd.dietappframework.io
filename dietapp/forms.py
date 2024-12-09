35e2a0ee6c88ce07b654da948aa2406ff7294095
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-09 06:03:13 -0800

    Update forms.py

--- dietapp/forms.py
+++ dietapp/forms.py
@@ -12,7 +12,7 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 from django import forms
 from .models import (
-    Meal, Vitamin, Mineral, Exercise, Weekly, JournalEntry, UserProfile, TDEE, HealthData
+    Meal, Vitamin, Mineral, Exercise, Weekly, JournalEntry, UserProfile, TDEE, HealthData, Profile
 )
 
 
