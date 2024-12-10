838c485a28d6cfbbb91878714ae524f029badc6d
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-10 04:43:16 -0800

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,27 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
-from django.db import models
-from django.contrib.auth.models import User
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
-    age = models.PositiveIntegerField(null=True, blank=True)
-    height = models.FloatField(null=True, blank=True)  # Height in centimeters
-    weight = models.FloatField(null=True, blank=True)  # Weight in kilograms
-    goal = models.CharField(
-        max_length=100,
-        choices=[
-            ('lose_weight', 'Lose Weight'),
-            ('gain_weight', 'Gain Weight'),
-            ('maintain_weight', 'Maintain Weight')
-        ],
-        default='maintain_weight'
-    )
-
-    def __str__(self):
-        return f"{self.user.username}'s Profile"
-
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -30,15 +9,25 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    """
-    Changes here explained below
-    """
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
 
         img = Image.open(self.image.path)
 
+        # Resize the image if it is too large
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.PositiveIntegerField(null=True, blank=True)
+    height = models.FloatField(null=True, blank=True)
+    weight = models.FloatField(null=True, blank=True)
+    dietary_preferences = models.TextField(blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} UserProfile'
+        
