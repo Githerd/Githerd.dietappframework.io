cbbaf501de5d1b4673869f896729aa9c3197980a
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-10 01:01:48 -0800

    Update models.py

--- dietapp/models.py
+++ dietapp/models.py
@@ -8,8 +8,7 @@ from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
-from .models import Meal, Mineral, Vitamin
-
+from .models import apps
 
 User = get_user_model()
 
