ca291128f2316c7dc824344d27242546d65812c5
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-10 12:21:38 -0800

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,41 +1,24 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.core.validators import MinValueValidator, FileExtensionValidator
-from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractUser, User
+from django.core.validators import FileExtensionValidator
 from PIL import Image
 
 # Utility function for user file upload paths
 def user_directory_path(instance, filename):
-    return f'profile_pics/{instance.username}/{filename}'
+    return f'profile_pics/{instance.user.username}/{filename}'
 
-# Custom User Model
-class User(AbstractUser):
-    email = models.EmailField(unique=True)
-    date_joined = models.DateTimeField(auto_now_add=True)
+# Profile Model
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     image = models.ImageField(
         default='default.jpg',
         upload_to=user_directory_path,
-        validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])],
+        validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])]
     )
     age = models.IntegerField(null=True, blank=True)
     weight = models.FloatField(null=True, blank=True)
     height = models.FloatField(null=True, blank=True)
     dietary_preferences = models.TextField(null=True, blank=True)
-
-    def __str__(self):
-        return self.username
-
-    @property
-    def bmi(self):
-        if self.height and self.weight:
-            height_in_meters = self.height / 100
-            return round(self.weight / (height_in_meters ** 2), 2)
-        return None
-
-# Profile Model
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
@@ -51,3 +34,10 @@ class Profile(models.Model):
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
+
+    @property
+    def bmi(self):
+        if self.height and self.weight:
+            height_in_meters = self.height / 100
+            return round(self.weight / (height_in_meters ** 2), 2)
+        return None
