65dab1f4fa8ccb8c59124253bb72bfdb57adec4a
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-09 20:42:16 -0800

    Update views.py

--- dietapp/views.py
+++ dietapp/views.py
@@ -10,7 +10,7 @@ from django.utils.decorators import method_decorator
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from .models import (
-    Meals,
+    Meal,
     Exercise,
     Weekly,
     TDEE,
