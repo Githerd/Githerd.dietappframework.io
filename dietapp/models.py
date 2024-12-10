c296e6466369bc9b647482291fd51762cdcc54ff
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-10 06:32:37 -0800

    Update models.py

--- dietapp/models.py
+++ dietapp/models.py
@@ -1,9 +1,9 @@
 from django import forms
 from django.db import models
 from django.db.models import TextChoices
-from django.contrib.auth.models import User, Profile, AbstractUser, Carbs, Drinks, Fats, Meals, Proteins, User, Weekly, Meakl, JournalEntry, TDEE, Exercise 
+from django.db import models
+from django.contrib.auth import get_user_model
 from django.utils.timezone import now
-from django.core.validators import MinValueValidator, FileExtensionValidator
 from django.core.exceptions import ValidationError
 from django.urls import reverse
 from django.conf import settings
@@ -16,38 +16,6 @@ User = get_user_model()
 # Utility function for user file upload paths
 def user_directory_path(instance, filename):
     return f'profile_pics/{instance.user.username}/{filename}'
-
-
-# Custom User Model
-class User(AbstractUser):
-    email = models.EmailField(unique=True)
-    date_joined = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.username
-
-
-# User Profile Model
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-    age = models.IntegerField(null=True, blank=True)
-    weight = models.FloatField(null=True, blank=True)
-    height = models.FloatField(null=True, blank=True)
-    dietary_preferences = models.TextField(null=True, blank=True)
-
-    def __str__(self):
-        return f'{self.user.username} Profile'
-
-
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    age = models.IntegerField(null=True, blank=True)
-    height = models.FloatField(null=True, blank=True)
-    weight = models.FloatField(null=True, blank=True)
-    dietary_preferences = models.TextField(null=True, blank=True)
-
 
     
     @property
