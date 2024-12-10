22a4b5b95bc20d2ea0c154b4831215a38fca6616
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-09 19:25:19 -0800

    Update views.py

--- users/views.py
+++ users/views.py
@@ -1,41 +1,39 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages #import for messages
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import login, authenticate
-from django.contrib import messages
-from .forms import UserRegisterForm, UserProfileForm
-from .models import UserProfile
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 def register(request):
-    """Handle user registration."""
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
-        profile_form = UserProfileForm(request.POST, request.FILES)
-        if form.is_valid() and profile_form.is_valid():
-            user = form.save()
-            profile = profile_form.save(commit=False)
-            profile.user = user
-            profile.save()
-            login(request, user)
-            messages.success(request, "Registration successful! Welcome to DietApp.")
-            return redirect('dietapp-home')
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'The account {username} was created successfully, now you can login.')
+            return redirect('login')
     else:
         form = UserRegisterForm()
-        profile_form = UserProfileForm()
-    return render(request, 'users/register.html', {'form': form, 'profile_form': profile_form})
-
+    return render(request, 'users/register.html', {'form': form})
 
 @login_required
 def profile(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile_image)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
-            messages.success(request, 'Your profile has been updated!')
+            messages.success(request, f'Your account has been updated')
             return redirect('profile')
     else:
         u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.user.profile_image)
-    return render(request, 'users/profile.html', {'u_form': u_form, 'p_form': p_form})
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'users/profile.html', context)
