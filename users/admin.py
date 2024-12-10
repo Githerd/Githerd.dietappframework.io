3ae1f8ce8b249484173a732b58b6b0a74d8cd02e
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-10 12:47:34 -0800

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
-from .models import Profile
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Profile
 
-# Register your models here.
-admin.site.register(Profile)
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    search_fields = ('username', 'email')
+    list_filter = ('is_staff', 'is_active')
+    ordering = ('username',)
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'age', 'weight', 'height', 'bmi')  # Display additional fields
+    search_fields = ('user__username', 'user__email')  # Enable search by username or email
+    list_filter = ('age',)  # Add filtering by age
+    readonly_fields = ('bmi',)  # Make BMI read-only
+
+    def bmi(self, obj):
+        return obj.bmi
+    bmi.short_description = 'BMI' 
