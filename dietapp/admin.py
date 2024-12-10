9d6de5f162fc9766448cfa6309e155489ba0ee16
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-09 17:20:00 -0800

    Update admin.py

--- dietapp/admin.py
+++ dietapp/admin.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
 from .models import (
-    Meal, Carbs, Drinks, Fats, Vitamin, Proteins,
-    User, Mineral, Exercise, Weekly, JournalEntry, UserProfile, TDEE
+    Meal, Carbs, Drinks, Fats, Vitamin, Proteins, User, Mineral, Exercise, Weekly, JournalEntry, UserProfile, TDEE
 )
 
 # Register all models in a single call for better clarity
 models_to_register = [
-    User, Carbs, Fats, Proteins, Drinks, Meal, Exercise,
- Weekly, TDEE, JournalEntry, Vitamin, UserProfile, Mineral, Message
+    User, Carbs, Fats, Proteins, Drinks, Meal, Exercise, Weekly, TDEE, JournalEntry, Vitamin, UserProfile, Mineral, Message
 ]
 
 # Register models in the admin panel
