b3a0a44cc557e6b74d1414e316dbc49ae95281e5
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-10 04:18:51 -0800

    Update forms.py

--- users/forms.py
+++ users/forms.py
@@ -4,7 +4,8 @@ from django.contrib.auth.forms import UserCreationForm #Inheritance Relationship
 
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
-from .models import Profile
+from .models import Profile, UserProfile
+
 User = get_user_model()
 
 class UserRegisterForm(UserCreationForm):
@@ -30,4 +31,15 @@ class UserUpdateForm(forms.ModelForm):
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['image']
+        fields = ['image', 'age', 'height', 'weight', 'goal']
+        widgets = {
+            'goal': forms.Select(attrs={'class': 'form-control'}),
+            'age': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter your age'}),
+            'height': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter your height in cm'}),
+            'weight': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter your weight in kg'}),
+        }
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['age', 'height', 'weight', 'dietary_preferences']
