15d172e170ae92d4e6371dba1729d94914d16525
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-09 19:56:56 -0800

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,6 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    age = models.PositiveIntegerField(null=True, blank=True)
+    height = models.FloatField(null=True, blank=True)  # Height in centimeters
+    weight = models.FloatField(null=True, blank=True)  # Weight in kilograms
+    goal = models.CharField(
+        max_length=100,
+        choices=[
+            ('lose_weight', 'Lose Weight'),
+            ('gain_weight', 'Gain Weight'),
+            ('maintain_weight', 'Maintain Weight')
+        ],
+        default='maintain_weight'
+    )
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
