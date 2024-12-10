41f48468d97f6e7d8e8f0269a152a34a64caa7d2
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-10 09:41:13 -0800

    Update admin.py

--- dietapp/admin.py
+++ dietapp/admin.py
@@ -6,8 +6,16 @@ class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'age', 'height', 'weight')
 
 # Register all models in a single call for better clarity
-#models_to_register = [
-    User, Carbs, Fats, Proteins, Drinks, Meal, Exercise, Weekly, TDEE, JournalEntry, Vitamin, UserProfile, Mineral, Message
+models_to_register = [
+    UserProfile,
+    Meal,
+    Weekly,
+    Vitamin,
+    Mineral,
+    Exercise,
+    TDEE,
+    JournalEntry,
+    Message
 ]
 
 # Register models in the admin panel
