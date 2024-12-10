0cf047b6ba1983d6b8d1b13141bc0a90da44254c
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-10 07:05:06 -0800

    Update admin.py

--- dietapp/admin.py
+++ dietapp/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from dietapp.models import (
-    Meal, Carbs, Drinks, Fats, Vitamin, Proteins, User, Mineral, Exercise, Weekly, JournalEntry, UserProfile, TDEE, Message
-)
+from dietapp.models import UserProfile
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'age', 'height', 'weight')
 
 # Register all models in a single call for better clarity
-models_to_register = [
+for models_to_register = [
     User, Carbs, Fats, Proteins, Drinks, Meal, Exercise, Weekly, TDEE, JournalEntry, Vitamin, UserProfile, Mineral, Message
 ]
 
