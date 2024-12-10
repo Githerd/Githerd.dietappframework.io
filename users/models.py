debe9877b66dab97a68fa17f41d5a968e566becb
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-10 11:24:30 -0800

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,27 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import MinValueValidator, FileExtensionValidator
-from django.urls import reverse
+from django.core.validators import MinValueValidator
 from django.utils.translation import gettext_lazy as _
-from PIL import Image
 
-# User Model
+# Utility function for user file upload paths
+def user_directory_path(instance, filename):
+    return f'profile_pics/{instance.username}/{filename}'
+
+
+# Custom User Model
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     date_joined = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.username
-
-
-# User Profile Model
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    image = models.ImageField(
+        default='default.jpg',
+        upload_to=user_directory_path,
+        validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])],
+    )
     age = models.IntegerField(null=True, blank=True)
     weight = models.FloatField(null=True, blank=True)
     height = models.FloatField(null=True, blank=True)
     dietary_preferences = models.TextField(null=True, blank=True)
 
     def __str__(self):
-        return f'{self.user.username} Profile'
+        return self.username
+
+    @property
+    def bmi(self):
+        if self.height and self.weight:
+            height_in_meters = self.height / 100
+            return round(self.weight / (height_in_meters ** 2), 2)
+        return None
