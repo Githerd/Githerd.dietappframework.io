22e0a88bc8664b140a78c5d93d9591afa561bf2e
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-10 15:19:44 -0800

    Update apps.py

--- users/apps.py
+++ users/apps.py
@@ -1,8 +1,18 @@
-from django.apps import AppConfig
+# users/signals.py
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth import get_user_model
+from .models import Profile
 
-class UsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'
+User = get_user_model()
 
-    def ready(self):
-        import users.signals
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    """Create a Profile when a new User is created."""
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    """Save the Profile when the User is saved."""
+    instance.profile.save()
