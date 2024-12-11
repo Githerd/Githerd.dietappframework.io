b4ffa53dd50f41c3744ed1f054605251d412ee8c
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-11 13:47:58 -0800

    Update models.py

--- dietapp/models.py
+++ dietapp/models.py
@@ -9,7 +9,16 @@ from django.db.models import TextChoices
 
 User = get_user_model()  # Use this dynamically to reference the custom user model
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    image = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    age = models.IntegerField(null=True, blank=True)
+    weight = models.FloatField(null=True, blank=True)
+    height = models.FloatField(null=True, blank=True)
 
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+        
 class FoodComponent(models.Model):
     name = models.CharField(max_length=50)
     gfat = models.PositiveIntegerField(default=0, verbose_name="Fat (g)")
