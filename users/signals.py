fd1b57247ee4413f8240f2e208077b449bbec839
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-11 15:12:37 -0800

    Update signals.py

--- users/signals.py
+++ users/signals.py
@@ -1,18 +1,13 @@
 from django.db.models.signals import post_save
+from django.contrib.auth.models import User
 from django.dispatch import receiver
-from django.contrib.auth import get_user_model
-from users.models import Profile  # Import Profile directly here
-
-User = get_user_model()
+from .models import Profile
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
-    """Create a Profile when a new User is created."""
     if created:
         Profile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
-    """Save the Profile when the User is saved."""
-    if hasattr(instance, 'profile'):
-        instance.profile.save()
+    instance.profile.save()
