77424f80568d55372f01e76519e6cac2a5012c94
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-11 15:28:55 -0800

    Update models.py

--- users/models.py
+++ users/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 from django.core.validators import FileExtensionValidator
 from PIL import Image
 
@@ -10,13 +10,20 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
 
+
 # Utility function for user file upload paths
 def user_directory_path(instance, filename):
+    """Generate file path for user profile image."""
     return f'profile_pics/{instance.user.username}/{filename}'
+
 
 # Profile Model
 class Profile(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='profile')
+    user = models.OneToOneField(
+        CustomUser, 
+        on_delete=models.CASCADE, 
+        related_name='profile'
+    )
     image = models.ImageField(
         default='default.jpg',
         upload_to=user_directory_path,
@@ -34,15 +41,16 @@ class Profile(models.Model):
         """Override save method to resize image before saving."""
         super().save(*args, **kwargs)
 
-        img = Image.open(self.image.path)
-        if img.height > 300 or img.width > 300:
-            output_size = (300, 300)
-            img.thumbnail(output_size)
-            img.save(self.image.path)
+        if self.image:
+            img = Image.open(self.image.path)
+            if img.height > 300 or img.width > 300:
+                output_size = (300, 300)
+                img.thumbnail(output_size)
+                img.save(self.image.path)
 
     @property
     def bmi(self):
-        """Calculate BMI."""
+        """Calculate and return BMI (Body Mass Index)."""
         if self.height and self.weight:
             height_in_meters = self.height / 100
             return round(self.weight / (height_in_meters ** 2), 2)
