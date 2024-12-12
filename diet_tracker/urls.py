17763d77f450209962c9ac0c3eb8c7135aa82c76
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-11 16:17:17 -0800

    Update urls.py

--- diet_tracker/urls.py
+++ diet_tracker/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.db import models
+from . import views
 from django.contrib.auth import views as auth_views
 from django.conf import settings  # For media files handling
 from django.conf.urls.static import static
