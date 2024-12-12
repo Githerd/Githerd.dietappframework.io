43915be4a645ea6ff253ce446ef31edc3d30beb2
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-11 18:58:58 -0800

    Update forms.py

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm, PasswordResetForm, SetPasswordForm, CustomSetPasswordForm, CustomPasswordResetForm
+from django.contrib.auth.forms import UserCreationForm, PasswordResetForm, SetPasswordForm, 
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 from .models import Profile
