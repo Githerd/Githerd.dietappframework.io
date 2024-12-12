7694e93c84d9457b0a869ee41a00e86f034d24c8
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-11 19:09:18 -0800

    Update apps.py

--- dietapp/apps.py
+++ dietapp/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class DietappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dietapp'
-    
-app.listen(port, () => {
-  console.log(`dietapp listening on port ${port}`)
-})
