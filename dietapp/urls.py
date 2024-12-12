fba9cabeb6325b0497f322a0f6ce6427d078b1d4
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-12 04:34:18 -0800

    Update urls.py

--- dietapp/urls.py
+++ dietapp/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 from django.contrib import admin
 from users import views as user_views
 from . import views
-from django.urls import path
 from .views import JournalListView, JournalDetailView, JournalCreateView, JournalUpdateView, JournalDeleteView, TDEEView
 
 
