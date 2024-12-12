b0c4d92f9f5502f9880aca557edfcf7ffd9f4ce9
Githerd <158222320+Githerd@users.noreply.github.com>
2024-12-11 17:49:14 -0800

    Update urls.py

--- diet_tracker/urls.py
+++ diet_tracker/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("mealplanmaker.urls")),
+    path("", include("dietapp.urls")),
 ]
 
 
